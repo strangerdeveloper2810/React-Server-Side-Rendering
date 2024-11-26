58f124e4a2a07ccea81c825f0f6b17f24f41db8d
Trình Nguyễn <nguyenmaster3012@gmail.com>
2024-11-27 02:51:54 +0700

    adding wait-on

--- src/client/pages/Home/Home.tsx
+++ src/client/pages/Home/Home.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home: React.FC = (): JSX.Element => {
-  return <>Config React SSR With Express</>;
+  return <>Config React SSR With Express.js server âsdashdjkahsdjkh</>;
 };
 
 export default Home;
